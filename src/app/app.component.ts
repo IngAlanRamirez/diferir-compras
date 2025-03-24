4908098a1ffccef561d7644cb936b6dfadaf4bde
Alan Christian Ramírez Rodríguez <ingenieroacramirez@gmail.com>
2025-03-24 17:57:39 -0600

    agregar componente de pestañas y actualizar el diseño de la barra de herramientas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { TabsComponent } from './tabs/tabs.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatIconModule],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, TabsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
